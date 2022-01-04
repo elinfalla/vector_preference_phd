e1c0e9035d24902273d4a8a5d3554aa3743bc569
Elin Falla <elinfalla@me.com>
2022-01-04 16:22:41 +0000

    improve comments for donnelly_determ.R

--- model_comparison/donnelly_determ.R
+++ model_comparison/donnelly_determ.R
@@ -1,6 +1,6 @@
 
 #######################################################################
-##### RECREATION OF STOCHASTIC SIMULATION BY DONNELLY ET AL. 2019 #####
+###### RECREATION OF DETERMINISTIC MODEL BY DONNELLY ET AL. 2019 ######
 #######################################################################
 
 rm(list=ls())
@@ -29,7 +29,7 @@ parms <- c(
 
 #states
 init_states <- c(
-i = 1/parms[["H"]],# frequency of infected plants
+i = 1/parms[["H"]], # frequency of infected plants
 As = 1200 / parms[["H"]] / 2, # per plant aphid density on average healthy plant
 Ai = 1200 / parms[["H"]] / 2 # per plant aphid density on average infected plant
 )
@@ -40,12 +40,12 @@ times <- seq(0, 10, by = 0.2)
 # define ode function
 donnelly_ode_func <- function(times, states, parms) {
   
-  #states
+  # STATES
   i <- states[["i"]]
   As <- states[["As"]]
   Ai <- states[["Ai"]]
   
-  # parameters
+  # PARAMETERS
   gamma <- parms[["gamma"]]
   b <- parms[["b"]]
   theta <- parms[["theta"]]
@@ -59,32 +59,54 @@ donnelly_ode_func <- function(times, states, parms) {
   Pacq <- parms[["Pacq"]]
   Pinoc <- parms[["Pinoc"]]
   
-  # set up equations for state equations
-  q <- 1 - p  # q = prob of surviving
   
-  i_hat <- v*i / ((1-i) + v*i)
+  # DEFINE PARAMETERS NEEDED FOR STATE EQUATIONS
   
+  # q = prob of surviving flight between plants (i.e. doesn't emigrate/die)
+  q <- 1 - p  
+  
+  # i_hat = weighted frequency of infected plants, accounting for attraction towards infected plants
+  # i.e. adapted version of i / (1 - i + i) = i / 1, to include v
+  i_hat <- v*i / ((1-i) + v*i) 
+  
+  # expected number of transmissions per dispersal, 
+  # derived from analysis of markov chain (see Donnelly 2019, Appendix S1)
   xi <- (q^2*Pacq*Pinoc*i_hat*(1 - e*w)*(1 - i_hat))/(p + q*w*(1 - i_hat*(1 - e)))
   
-  Fs <- (1 - i_hat) / (1 - i_hat*(1 - e))
+  # probability of settling on susceptible (Fs) and infected (Fi) plants,
+  # derived from analysis of Markov chain, see Donnelly et al. 2019, Appendix S4
+  Fs <- (1 - i_hat) / (1 - i_hat*(1 - e)) 
   Fi <- e*i_hat / (1 - i_hat*(1 - e))
   
-  A <- As*(H - i*H) + Ai*(i*H)  # A = total number of aphids i.e. aphids per plant type * number of that plant type
+  # A = total number of aphids i.e. aphids per plant type * number of that plant type
+  # i.e. S*As + I*Ai where S is number of susceptible plants and I is number of infected plants
+  A <- As*(H - i*H) + Ai*(i*H)  
+  
   
   # STATE EQUATIONS
-  # aphids
+  # aphids - change in per plant aphid density on susceptible (As)  and infected (Ai) plants
+  # = aphid births - aphid deaths - aphid settling on other plant type + aphids moving from other plant type
   dAs <- a*As*(1 - As/K) - b*As - theta*As*(1 - Fs) + theta*Ai*Fs*i/(1 - i)
   dAi <- a*Ai*(1 - Ai/K) - b*Ai - theta*Ai*(1 - Fi) + theta*As*Fi*(1 - i)/i
 
-  # infected plants
+  # infected plants - change in incidence of infected plants
+  # = rate of aphid dispersal * mean number of transmissions - plant death
   di <- (theta*A/H)*xi - gamma*i
   
   return(list(c(di, dAs, dAi)))
 }
 
+
 vary_param_trajec <- function(init_states, times, parms, varied_parm_name, varied_parm_vals) {
   
-  list_of_trajects <- vector(mode = "list", length = length(varied_parm_vals))
+  # function to run the ODE multiple times with one parameter varying, as specified by user.
+  # returns data frame of all runs
+  
+  # initialise output data frame
+  all_trajects <- data.frame(time = numeric(),
+                             compartment = character(),
+                             value = numeric(),
+                             param_val = numeric())
   
   for (run in 1:length(varied_parm_vals)) {
     
@@ -96,15 +118,20 @@ vary_param_trajec <- function(init_states, times, parms, varied_parm_name, varie
                                  func = donnelly_ode_func))
     
     trajectory_long <- reshape2::melt(trajectory, id.vars="time")
-    names(trajectory_long) <- c("time", "compartment", "number")
     
-    list_of_trajects[[run]] <- trajectory_long
+    # add the value of the varying parameter as an extra column
+    trajectory_long$param_val <- rep(varied_parm_vals[run], times=nrow(trajectory_long))
+    
+    # add to dataframe of all trajectories
+    all_trajects <- rbind(all_trajects, trajectory_long)
   }
   
-  return(list_of_trajects)
+  return(all_trajects)
 }
 
+########
 # run epidemic with default parameters (as donnelly et al. 2019)
+########
 trajectory <- data.frame(ode(y = init_states, 
                              times = times, 
                              parms = parms, 
@@ -125,20 +152,29 @@ aphid_trajec <- ggplot(data=trajectory_long %>% filter(!(compartment == "i")),
   geom_line()
 aphid_trajec
 
-# run epidemic with varying values of e and w
+##########
+# run epidemic with varying values of e and v
+##########
+
+#VARY E (plant acceptability)
 e_vals <- c(0.25, 0.5, 1, 1.5, 2)
 
 e_trajecs <- vary_param_trajec(init_states, times, parms, "e", e_vals)
-e_trajecs_i_only <- list(e_trajecs[[1]] %>% filter(compartment == "i"),
-                         e_trajecs[[2]] %>% filter(compartment == "i"),
-                         e_trajecs[[3]] %>% filter(compartment == "i"),
-                         e_trajecs[[4]] %>% filter(compartment == "i"),
-                         e_trajecs[[5]] %>% filter(compartment == "i"))
 
-vary_e_plot <- ggplot(data=e_trajecs_i_only[[1]], aes(x = time, y = number)) +
-  geom_line(col="red") +
-  geom_line(data=e_trajecs_i_only[[2]], col="orange") +
-  geom_line(data=e_trajecs_i_only[[3]], col="yellow") +
-  geom_line(data=e_trajecs_i_only[[4]], col="green") +
-  geom_line(data=e_trajecs_i_only[[5]], col="blue")
-vary_e_plot
+# plot output
+e_vals_plot <- ggplot(data=e_trajecs %>% filter(variable == "i"), aes(x = time, y = value, col = as.factor(param_val))) +
+  geom_line() +
+  labs(col = "e value")
+e_vals_plot
+
+# VARY V (plant attractiveness)
+v_vals <- c(1, 3, 0.6, 0.55)
+
+long_times <- seq(0, 20, by=0.5)
+v_trajecs <- vary_param_trajec(init_states, long_times, parms, "v", v_vals)
+
+# plot output
+v_vals_plot <- ggplot(data=v_trajecs %>% filter(variable == "i"), aes(x = time, y = value, col = as.factor(param_val))) +
+  geom_line() +
+  labs(col = "v value")
+v_vals_plot
