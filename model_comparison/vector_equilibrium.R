1a088501d48e0e77b335f91986dd17a1fe79cf15
Elin Falla <elinfalla@me.com>
2022-01-06 14:08:47 +0000

    implement desolve and optim methods of finding equilibriums in vector_equilibrium.R

--- model_comparison/vector_equilibrium.R
+++ model_comparison/vector_equilibrium.R
@@ -3,24 +3,46 @@
 ###### DETERMINING APHID POPULATION EQUILIBRIUM -  FROM DONNELLY ET AL. 2019 ######
 ###################################################################################
 
+# packages
+library(deSolve)
+library(ggplot2)
+library(reshape2)
+library(dplyr)
+
+
+##############
 ### Aphid population ODE solve
+##############
 
 parms <- c(
-  i = 0.5, # proportion of host plants that are infected
+  i = 0.1, # proportion of host plants that are infected
   v = 1, # infected plant attractiveness
   e = 1, # infected plant acceptability 
   a = 2, # reproduction rate per aphid per day
   b = 0.1, # aphid mortality rate per day
   K = 10, # aphid reproduction limit - maximum aphids per plant (diff to donnelly_stoch_sim.R)
-  theta = 1, # aphid dispersal rate per day
-  
+  theta = 1 # aphid dispersal rate per day
 )
 
+init_states <- c(
+  As = 12, # per plant aphid density on average healthy plant
+  Ai = 12 # per plant aphid density on average infected plant
+)
 
-
-aphid_pop_ODE <- function(times, y, parms) {
+aphid_pop_ode <- function(times, y, parms) {
   
+  # define states
+  As <- y[["As"]]
+  Ai <- y[["Ai"]]
   
+  # define parameters
+  i <- parms[["i"]]
+  v <- parms[["v"]]
+  e <- parms[["e"]]
+  a <- parms[["a"]]
+  b <- parms[["b"]]
+  K <- parms[["K"]]
+  theta <- parms[["theta"]]
   
   # i_hat = weighted frequency of infected plants, accounting for attraction towards infected plants
   # i.e. adapted version of i / (1 - i + i) = i / 1, to include v
@@ -37,5 +59,110 @@ aphid_pop_ODE <- function(times, y, parms) {
   dAs <- a*As*(1 - As/K) - b*As - theta*As*(1 - Fs) + theta*Ai*Fs*i/(1 - i)
   dAi <- a*Ai*(1 - Ai/K) - b*Ai - theta*Ai*(1 - Fi) + theta*As*Fi*(1 - i)/i
   
+  return(list(c(dAs, dAi)))
 }
 
+vary_init_states <- function(init_states_list, times, parms) {
+  
+  # function to run the ODE multiple times with the initial states varying, as specified by user.
+  # returns data frame of all runs. init_states_list is a list of vectors with the initial conditions
+  # for each run
+  
+  # initialise output data frame
+  all_trajects <- data.frame(time = numeric(),
+                             compartment = character(),
+                             value = numeric(),
+                             run_num = numeric())
+  
+  for (run in 1:length(init_states_list)) {
+    
+    trajectory <- data.frame(ode(y = init_states_list[[run]], 
+                                 times = times, 
+                                 parms = parms, 
+                                 func = aphid_pop_ode))
+    
+    trajectory_long <- reshape2::melt(trajectory, id.vars="time")
+
+    trajectory_long$run_num <- rep(run, nrow(trajectory_long))
+      
+    # add to dataframe of all trajectories
+    all_trajects <- rbind(all_trajects, trajectory_long)
+  }
+  
+  return(all_trajects)
+}
+
+init_states_list <- list(c(As = 1, Ai = 0),
+                        c(As = 3, Ai = 2),
+                        c(As = 5, Ai = 4),
+                        c(As = 7, Ai = 6),
+                        c(As = 9, Ai = 8),
+                        c(As = 11, Ai = 10),
+                        c(As = 13, Ai = 12),
+                        c(As = 15, Ai = 14),
+                        c(As = 17, Ai = 16))
+
+
+aphid_trajecs <- vary_init_states(init_states_list, times, parms)
+
+
+
+aphid_plot <- ggplot(data = aphid_trajecs %>% filter(run_num == 1), aes(x = time, y = value, col = variable)) +
+  geom_line() +
+  labs(title = "Aphid population ODE solve")
+
+num_runs <- length(init_states_list)
+
+for (run in 1:(num_runs - 1)) {
+  aphid_plot <- aphid_plot +
+    geom_line(data = aphid_trajecs %>% filter(run_num == run))
+    
+}
+
+aphid_plot
+
+
+
+##############
+### Find aphid population equilibrium using optim()
+##############
+
+RHS_to_optimise <- function(aphid_pop_density, parms) {
+  
+  ### Function that contains the equations for As and Ai to be optimised. Will be passed to optim()
+  
+  As <- aphid_pop_density[1]
+  Ai <- aphid_pop_density[2]
+  
+  # define parameters
+  i <- parms[["i"]]
+  v <- parms[["v"]]
+  e <- parms[["e"]]
+  a <- parms[["a"]]
+  b <- parms[["b"]]
+  K <- parms[["K"]]
+  theta <- parms[["theta"]]
+  
+  # equations
+  i_hat <- v*i / ((1-i) + v*i) 
+  
+  Fs <- (1 - i_hat) / (1 - i_hat*(1 - e)) 
+  Fi <- e*i_hat / (1 - i_hat*(1 - e))
+  
+  # equations to be minimised
+  dAs <- a*As*(1 - As/K) - b*As - theta*As*(1 - Fs) + theta*Ai*Fs*i/(1 - i)
+  dAi <- a*Ai*(1 - Ai/K) - b*Ai - theta*Ai*(1 - Fi) + theta*As*Fi*(1 - i)/i
+  
+  return(dAs^2 + dAi^2) # return sum of squares. this way the minimum value is 0 which indicates a root (equilibrium)
+}
+
+# run optim() many times from different starting values to find all possible equilibriums
+n_tests <- 100
+equilibrium <- lapply(1:n_tests, function(x) round(optim(par = runif(2, min=0, max=100), 
+                                                fn = RHS_to_optimise, 
+                                                parms = parms,
+                                                method = "L-BFGS-B")$par, 4))
+
+# filter down to unique equilibriums
+unique_equilibrium <- equilibrium[!duplicated(equilibrium)]
+unique_equilibrium # only equilibrium where both numbers (As and Ai) are positive = 9.5, 9.5
